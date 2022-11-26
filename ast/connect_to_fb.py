26da924d4338228bd007d2bd5813b08960807e74
Borsh1992 <73963662+BorisLasker@users.noreply.github.com>
2022-11-26 12:00:35 +0200

    1

--- ast/connect_to_fb.py
+++ ast/connect_to_fb.py
@@ -6,11 +6,12 @@ cred = credentials.Certificate("firebase-sdk.json")
 firebase_admin.initialize_app(cred,{
     'databaseURL': 'https://mediashare-72f12-default-rtdb.firebaseio.com/'
 })
-ref = db.reference('User')
+ref = db.reference('message')
 user_ref = ref.push({
-                      
+              
+    'currentDateTime':"21 בנוב׳ 2022 10:19:21",     
     'email':"boris.laskerr@gmail.com",
-    'password':"123",
+    'imageUrl':"https://www.fao.org/images/devforestslibraries/default-album/forests.jpg?sfvrsn=2dd96b96_11",
     'username':"Bor La"
 
  })
